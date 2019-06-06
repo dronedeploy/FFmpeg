1bbb20a5ccf49c1dbdf2aef494d63e7feaa5aefd
Devin Lane <devin@dronedeploy.com>
2019-06-06 13:58:20 -0600

    cleanup build scripts

--- build_ios_xcode.sh
+++ build_ios_xcode.sh
@@ -71,33 +71,3 @@ for ARCH in $ARCHS; do
 	make -j8 install $EXPORT || exit 1
 done
 
-#
-# Create fat libraries from each arch
-#
-
-#FAT=$BUILD_DIR/fat
-#echo "building fat binaries into $FAT"
-#mkdir -p $FAT/lib
-#
-#LIBDIR="$BUILD_DIR/install"
-#set - $ARCHS
-#cd "$LIBDIR/$1/lib"
-#LIBS="*.a"
-#for LIB in $LIBS; do
-#	echo lipo -create `find $LIBDIR -name $LIB` -output $FAT/lib/$LIB 1>&2
-#	lipo -create `find $LIBDIR -name $LIB` -output $FAT/lib/$LIB || exit 1
-#done
-#
-##
-## Create framework binary
-##
-#
-#PRODUCT_NAME=ffmpeg
-#FRAMEWORK="$BUILD_DIR/$PRODUCT_NAME.framework"
-#
-#mkdir -p "$FRAMEWORK"
-#
-#HEADER_DIR="$BUILT_PRODUCTS_DIR/$PUBLIC_HEADERS_FOLDER_PATH"
-#mkdir -p "$HEADER_DIR"
-#echo "$HEADER_DIR"
-#cp -rf $LIBDIR/$1/include/* "$HEADER_DIR"
